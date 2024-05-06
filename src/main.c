0b472d2c9c4885d5b80150491bd926999bd4f432
Chris Lumens <clumens@redhat.com>
2024-05-06 15:55:55 -0400

    main: Call crm_xml_init() if libpacemaker is being used.
    This is required to initialize various internal pacemaker XML structures
    and schemas before they are used the first time.  Without this, calling
    into libpacemaker will result in tracebacks.

--- src/main.c
+++ src/main.c
@@ -61,6 +61,9 @@
 #include <sys/prctl.h>
 #include <clplumbing/coredumps.h>
 #endif
+#ifdef LIBPACEMAKER
+#include <crm/common/xml.h>
+#endif
 #include "log.h"
 #include "booth.h"
 #include "config.h"
@@ -1625,6 +1628,10 @@ int main(int argc, char *argv[], char *envp[])
 	const char *cp;
 #ifdef LOGGING_LIBQB
 	enum qb_log_target_slot i;
+#endif
+
+#ifdef LIBPACEMAKER
+	crm_xml_init();
 #endif
 
 	init_set_proc_title(argc, argv, envp);
